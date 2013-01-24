0d2e5d95697e3cedb1d1a7032fd59bdb2f13befb
Holger Hans Peter Freyther <zecke@selfish.org>
2013-01-24 12:00:14 +0100

    sysmobts: Re-load the FPGA image and let some time pass before continuing
    We are not cleanly shutting down the DSP on exit so let us reset both
    the DSP and the FPGA. Also let some time pass to allow the DSP to properly
    initialize itself before starting to use it.
    
    This is not a fix but a band-aid for the unclean shutdown.

--- contrib/respawn.sh
+++ contrib/respawn.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 while [ -e /etc/passwd ]; do
+	cat /lib/firmware/sysmobts-v2.bit > /dev/fpgadl_par0
+	sleep 2s
 	cat /lib/firmware/sysmobts-v?.out > /dev/dspdl_dm644x_0
+	sleep 2s
 	echo "0" > /sys/class/leds/activity_led/brightness
 	nice -n -20 $*
+	sleep 8s
 done
