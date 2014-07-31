52c321faabd249968caa5bafd692e3221b36d710
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2014-07-31 21:16:57 +0200

    sysmobts: Read the temperature sensors on the device
    Read the sensors that are always present and the ones that
    are only present on the sysmoBTS 2050.

--- src/osmo-bts-sysmo/misc/sysmobts_mgr_vty.c
+++ src/osmo-bts-sysmo/misc/sysmobts_mgr_vty.c
@@ -96,6 +96,22 @@ DEFUN(cfg_mgr, cfg_mgr_cmd,
 DEFUN(show_mgr, show_mgr_cmd, "show manager",
       SHOW_STR "Display information about the manager")
 {
+	vty_out(vty, "Current Temperatures%s", VTY_NEWLINE);
+	vty_out(vty, " Digital: %f Celcius%s",
+		sysmobts_temp_get(SYSMOBTS_TEMP_DIGITAL,
+					SYSMOBTS_TEMP_INPUT) / 1000.0f,
+		VTY_NEWLINE);
+	vty_out(vty, " RF:      %f Celcius%s",
+		sysmobts_temp_get(SYSMOBTS_TEMP_RF,
+					SYSMOBTS_TEMP_INPUT) / 1000.0f,
+		VTY_NEWLINE);
+	if (is_sbts2050()) {
+		int temp_pa, temp_board;
+		sbts2050_uc_check_temp(&temp_pa, &temp_board);
+		vty_out(vty, " sysmoBTS 2050 PA: %d C%s", temp_pa, VTY_NEWLINE);
+		vty_out(vty, " sysmoBTS 2050 PA: %d C%s", temp_board, VTY_NEWLINE);
+	}
+
 	return CMD_SUCCESS;
 }
 
